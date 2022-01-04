75f3b8590a1730f3a6949655de918147552e62e8
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2022-01-04 13:43:03 -0800

    First fetches written successfully. Using Grant's code instead, as it's easier to read.

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,44 @@
+import { useEffect, useState } from 'react';
 import logo from '../logo.svg';
 import './App.css';
 
 function App() {
+  const [space, setSpace] = useState([])
+  const PLANETARY_URL = "https://api.le-systeme-solaire.net/rest/bodies/"
+  const PLANET_FILTER = "?filter[]=isPlanet,neq,false"
+  const WEATHER_URL = "https://api.weather.gov/points/"
+  const WEATHER_KEY = "data.properties.forecastGridData"
+
+
+  function getPlanets(){
+    fetch(PLANETARY_URL+PLANET_FILTER)
+    .then(r => r.json())
+    .then(data => console.log(data))
+  }
+
+  function getLocation(){
+    navigator.geolocation.getCurrentPosition(function(position) {
+      let lat = position.coords.latitude
+      let long = position.coords.longitude
+      let userCoords = `${lat},${long}`
+    }, function(error) {
+      alert(`${error.message},`)
+    })
+  }
+
+  function getWeather(){
+      fetch(WEATHER_URL+userCoords)
+      .then(r => r.json())
+      .then(data => console.log(data))
+  }
+
+  useEffect(()=>{
+    getPlanets();
+    getWeather();
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
