e3358f026dc439b5ead25b7a20848bc14a11bf67
Grant Pierce <glpierce174@gmail.com>
2022-01-05 23:41:54 -0800

    Edited data structures in db.json, wrote preliminary db.json fetch functions in DailyForecast.js (currently commented)

--- src/components/DailyForecast.js
+++ src/components/DailyForecast.js
@@ -17,3 +17,58 @@ function DailyForecast({ forecast }) {
 }
 
 export default DailyForecast
+
+
+
+// function getSavedLocationDataDB(coords) {
+//     fetch(`http://localhost:WXYZ/weatherData/${coords}`)
+//     .then(resp => resp.json())
+//     .then(data => setDetailData(data))
+// }
+
+// function weatherDataDBSetup () {
+//     getSavedLocationsDB(user)
+// }
+
+// function getSavedLocationsDB(user) {
+//     fetch(`http://localhost:WXYZ/users/${user}/savedLocations`)
+//     .then(resp => resp.json())
+//     .then(locations => getSavedLocationDataAPI(locations))  //locations: array of coordinate strings
+// }
+
+// function getSavedLocationDataAPI(locations) {
+//     const savedLocationData = locations.map(coords => getWeatherStation(coords)) //savedLocationData: array of objects - [{id: coords, grid:gridData, forecast:forecastData}, ...]
+//     savedLocationData.forEach(locationDataObj => postSavedLocationDataDB(locationDataObj)) 
+// }
+
+// function postSavedLocationDataDB(locationDataObj) {
+//     const postObj = {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify(locationDataObj)
+//     }
+//     fetch(`http://localhost:WXYZ/weatherData/`, postObj)
+// }
+
+// function deleteOneSavedLocationDataDB(coords) {
+//     const deleteObj = {
+//         method: "DELETE",
+//         headers: {
+//             "Content-Type": "application/json"
+//         }
+//     }
+//     fetch(`http://localhost:WXYZ/weatherData/${coords}`, deleteObj)
+// }
+
+// function deleteAllSavedLocationDataDB() {
+//     const putObj = {
+//         method: "PUT",
+//         headers: {
+//             "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify([])
+//     }
+//     fetch(`http://localhost:WXYZ/weatherData/${coords}`, putObj)
+// }
