e31512d6e59c5d43c769d63395fc94c417b3a87b
Grant Pierce <glpierce174@gmail.com>
2022-01-05 20:04:25 -0800

    Added geocooding fetch functinality, controlled search input to pass to geoocoding function on search submit

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,18 +1,27 @@
 import SearchBar from "./SearchBar"
 import LocationCards from "./LocationCards"
 import LocationDetail from "./LocationDetail"
-import { useState } from "react"
+import { useState, useEffect, useRef } from "react"
 import {Route, Switch} from 'react-router-dom'
 
 function Weather({ gridData, forecastData }) {
+
+    function getCoordinates(address) {
+        fetch(`https://geocode.xyz/?locate=${address}&json=1`)
+        .then(resp => resp.json())
+        .then(data => console.log(data))
+      }
+  
+      function getAddress(coordinates) {
+        fetch(`https://geocode.xyz/?locate=${coordinates}&json=1`)
+        .then(resp => resp.json())
+        .then(data => console.log(data))
+      }
     
     function degToCardinal(dir) {
         const directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
-      
         dir += 22.5;
-      
         dir < 0 ? dir = 360 - Math.abs(dir) % 360 : dir = dir % 360;
-      
         let wind = parseInt(dir / 45);
         return `${directions[wind]}`;
     }
@@ -20,7 +29,7 @@ function Weather({ gridData, forecastData }) {
     return(
         <div>
             {/* {apparentTemperature.values.map(value => <p>{value.validTime, value.value}</p>)} */}
-            <SearchBar />
+            <SearchBar newSearch={getCoordinates}/>
             <Switch>
                 <Route path= "/weather/saved">
                     <LocationCards gridData={gridData} degToCardinal={degToCardinal} />
