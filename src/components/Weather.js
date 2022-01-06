1fc9eede4d8f9f9e5538681f5ae607d4d78318b0
Grant Pierce <glpierce174@gmail.com>
2022-01-06 10:07:04 -0800

    A lot

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,19 +4,137 @@ import LocationDetail from "./LocationDetail"
 import { useState, useEffect, useRef } from "react"
 import { Route, Switch } from 'react-router-dom'
 
-function Weather({ gridData, forecastData }) {
+function Weather({ user }) {
+    const [{searchCoordinates, newSearch, savedSearch}, setSearchCoordinates] = useState({searchCoordinates: null, newSearch: false, savedSearch: false})
+    const [{coordinates, gridData, gridStatus, forecastData, forecastStatus}, setDetailData] = useState({coordinates: "", gridData: null, gridStatus: "idle", forecastData: null, forecastStatus: "idle"})
+    const [{cardData, cardStatus}, setCardData] = useState({cardData: {}, cardStatus: "pending"})
+    const [{cardsData, cardsStatus}, setCardsData] = useState({cardsData: [],  cardsStatus:"pending"})
 
-    function getCoordinates(address) {
-        fetch(`https://geocode.xyz/?locate=${address}&json=1`)
+    useEffect(() => { 
+        console.log(searchCoordinates)
+        if (newSearch) {
+            setDetailData(state => ({...state, coordinates: searchCoordinates, gridStatus: "pending", forecastStatus: "pending"}))
+            getWeatherStation(searchCoordinates)
+        } else if (savedSearch) {
+            const selectedCard = cardsData.find(card => card.coordinates === searchCoordinates)
+            console.log(selectedCard)
+            setDetailData(state => ({...state, coordinates: selectedCard.coordinates, gridData: selectedCard.grid, gridStatus: "fulfilled", forecastData: selectedCard.forecast, forecastStatus: "fulfilled"}))
+        }
+    }, [searchCoordinates])
+
+    useEffect(() => {
+        if (cardStatus === "fulfilled") {
+            setCardsData(state => ({...state, cardsData: [...cardsData, cardData]}))
+        } else {
+            getSavedLocationsDB(user)
+        }
+    }, [user, cardStatus])
+
+    function getSavedLocationsDB(user) {
+        fetch(`http://localhost:WXYZ/users/${user}/savedLocations`)
         .then(resp => resp.json())
-        .then(data => console.log(data))
-      }
+        .then(locations => {
+            if (locations.length > 0) {
+                locations.forEach(coords => {
+                    setCardData(state => ({...state, cardData: {...cardData, coordinates: coords}}))
+                    getWeatherStation(coords)
+                })
+            }
+        })  //locations: array of coordinate strings
+        .then(setCardsData(state => ({...state, cardsStatus: "fulfilled"})))
+    }
+
+    function getWeatherStation(coords) {
+        fetch(`https://api.weather.gov/points/${coords}`)
+        .then(resp => resp.json())
+        .then(respData => {getGridData(respData.properties.forecastGridData, respData.properties.forecast)})
+    }
+
+    function getGridData(gridURL, forecastURL) {
+        fetch(gridURL)
+        .then(resp => resp.json())
+        .then(data => {
+            let middleMan1 = {}
+            Object.keys(data.properties).forEach(key => {
+                typeof data.properties[key] === "object" && (middleMan1[key] = data.properties[key])
+            })
+            let middleMan2 = {}
+            Object.keys(middleMan1).forEach(key => {
+                const workingObj = {}
+                middleMan1[key].uom && (workingObj.uom = middleMan1[key].uom.split(":")[1]);
+                (middleMan1[key].values) ? workingObj.values = convertDateTimes(middleMan1[key].values) : (workingObj.value = middleMan1[key].value);
+                middleMan2[key] = workingObj
+            })
+            console.log("gridData fetched... ", middleMan2)
+            if (newSearch) {
+                setDetailData(state => ({...state, gridData: middleMan2, gridStatus: "fulfilled"}))
+                getForecastData(forecastURL)
+            } else {
+                setCardData(state => ({...state, cardData: {...cardData, grid: middleMan2}}))
+                getForecastData(forecastURL)
+            }
+        })
+    }  
+
+    function getForecastData(url) {
+        fetch(url)
+        .then(resp => resp.json())
+        .then(data => {
+            if (newSearch) {
+                setDetailData(state => ({...state, forecastData: data.properties.periods, forecastStatus: "fulfilled"}))
+            } else {
+                setCardData(state => ({...state, cardData: {...cardData, forecast: data.properties.periods, cardStatus: "fulfilled"}}))
+            }
+        })
+    }
+
   
-      function getAddress(coordinates) {
+    function saveLocation(coordinates, locationData) {
+        //push to server
+        console.log(locationData)
+        setCardsData(state => ({...state, cardsData: [...cardsData, locationData], cardsStatus: "fulfilled"}))
+        console.log(locationData, cardsData)
+    }
+
+
+    function getAddress(coordinates) {
         fetch(`https://geocode.xyz/?locate=${coordinates}&json=1`)
         .then(resp => resp.json())
         .then(data => console.log(data))
-      }
+    }
+
+    function convertDateTimes(valuesArray) {
+        const filteredValuesArray = []
+        valuesArray.forEach(valueObj => {
+            const rawDate = valueObj.validTime
+            const referenceWindow = parseDate(rawDate)
+            const windowEnd = new Date(referenceWindow[0].toString())
+            windowEnd.setHours(windowEnd.getHours()+referenceWindow[1])
+            const currentDateTime = new Date()
+            if (currentDateTime <= windowEnd) {
+                filteredValuesArray.push({...valueObj, validTime: referenceWindow[0]})
+            }
+        })
+        return filteredValuesArray
+    }
+
+    function parseDate (dateTimeString) {
+        if (!dateTimeString) {
+            return false
+        }
+        const dateArr = dateTimeString.split("/")
+        const cleanDate = new Date(dateArr[0])
+        let windowDuration 
+        if (dateArr[1].includes("T")) {
+            const step1 = dateArr[1].split("T")
+            windowDuration = parseInt(step1[1].split("H")[0])
+        } else {
+            const step1 = dateArr[1].split("P")
+            windowDuration = parseInt(step1[0].split("D")[0],10) * 24
+        }
+        return [cleanDate, windowDuration]
+    }
+
     
     function degToCardinal(dir) {
         const directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
@@ -29,13 +147,13 @@ function Weather({ gridData, forecastData }) {
     return(
         <div>
             {/* {apparentTemperature.values.map(value => <p>{value.validTime, value.value}</p>)} */}
-            <SearchBar newSearch={getCoordinates}/>
+            <SearchBar setSearchCoordinates={setSearchCoordinates} />
             <Switch>
                 <Route path= "/weather/saved">
-                    <LocationCards gridData={gridData} degToCardinal={degToCardinal} />
+                    {cardsStatus === "fulfilled" ? <LocationCards cardsData={cardsData} setSearchCoordinates={setSearchCoordinates} gridData={gridData} degToCardinal={degToCardinal} /> : <p>Loading...</p>}
                 </Route>
                 <Route path= "/weather/detail">
-                    <LocationDetail gridData={gridData} forecastData={forecastData} degToCardinal={degToCardinal} />
+                    {gridStatus === "fulfilled" && forecastStatus === "fulfilled" ? <LocationDetail coordinates={coordinates} gridData={gridData} forecastData={forecastData} saveLocation={saveLocation} degToCardinal={degToCardinal} /> : <p>Loading...</p>}
                 </Route>
             </Switch>
         </div>
